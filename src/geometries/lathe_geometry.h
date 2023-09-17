abef3beb10332ad9765cc0f1bbe0f35954fe8d70
nintymiles <nintymiles@icloud.com>
2023-09-17 21:29:18 +0800

    Create lathe_geometry.h

---
+++ src/geometries/lathe_geometry.h
@@ -0,0 +1,15 @@
+//
+// Created by SeanR on 2023/9/17.
+//
+
+#ifndef THREE_CPP_LATHE_GEOMETRY_H
+#define THREE_CPP_LATHE_GEOMETRY_H
+
+#include "buffer_geometry.h"
+
+class LatheGeometry:public BufferGeometry<int,double>{
+public:
+    LatheGeometry();
+};
+
+#endif //THREE_CPP_LATHE_GEOMETRY_H
