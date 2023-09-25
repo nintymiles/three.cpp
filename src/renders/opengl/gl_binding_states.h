7bdc71ee529db254611c1628223d8cfc0c1a890e
nintymiles <nintymiles@icloud.com>
2023-09-25 20:21:38 +0800

    Update gl_binding_states.h

--- src/renders/opengl/gl_binding_states.h
+++ src/renders/opengl/gl_binding_states.h
@@ -9,15 +9,39 @@
 #include "buffer_attribute.h"
 #include "gl_capabilities.h"
 
+//?主要用来管理vertex array的binding状态
 class GLBindingStates{
 public:
+    //named data members
+    std::shared_ptr<BufferGeometry<int,double>> geometry;
+    std::shared_ptr<GLProgram> program;
+    bool wireframe = false;
+    std::shared_ptr<BufferAttribute<int,double>> newAttributes;
+    std::shared_ptr<BufferAttribute<int,double>> enabledAttributes;
+
+
     int maxVertexAttributes;
     bool vaoAvailable = true;
+
+
+
 
 
     GLBindingStates(std::shared_ptr<BufferAttribute<int>> attribute, std::shared_ptr<GLCapabilities> capbilities){
         glGetIntegerv( GL_MAX_VERTEX_ATTRIBS,&maxVertexAttributes );
     }
+
+    void setup( Gluint vaoObject, material, program, geometry, index ) {
+
+    void bindVertexArrayObject( GLuint vao ) {
+        glBindVertexArray( vao );
+    }
+
+    void deleteVertexArrayObject( GLuint vao ) {
+        //glDeleteVertexArray( vao );
+        glDeleteVertexArrays(1,&vao);
+    }
+
 };
 
 
