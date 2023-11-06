9163959dc2a197d7eb3fbeeab7b2004108db97fb
nintymiles <nintymiles@icloud.com>
2023-11-07 07:53:31 +0800

    daily checkin 20231107 0753

--- src/curves/curve.h
+++ src/curves/curve.h
@@ -211,53 +211,42 @@ public:
 
     FrenetFrames computeFrenetFrames( int segments, bool  closed ) {
         // see http://www.cs.indiana.edu/pub/techreports/TR425.pdf
-        const normal = new Vector3();
+        Vector3 normal{};
 
-        const tangents = [];
-        const normals = [];
-        const binormals = [];
+        std::vector<Vector3> tangents{};
+        std::vector<Vector3> normals{};
+        std::vector<Vector3> binormals{};
 
-        const vec = new Vector3();
-        const mat = new Matrix4();
+        Vector3 vec{};
+        Matrix4 mat{};
 
         // compute the tangent vectors for each segment on the curve
-
-        for ( let i = 0; i <= segments; i ++ ) {
-
-            const u = i / segments;
-
-            tangents[ i ] = this.getTangentAt( u, new Vector3() );
-
+        for ( int i = 0; i <= segments; i ++ ) {
+            const float u = i / segments;
+            tangents.push_back(this->getTangentAt( u, Vector3()));
         }
 
         // select an initial normal vector perpendicular to the first tangent vector,
         // and in the direction of the minimum tangent xyz component
-
-        normals[ 0 ] = new Vector3();
-        binormals[ 0 ] = new Vector3();
-        let min = Number.MAX_VALUE;
-        const tx = Math.abs( tangents[ 0 ].x );
-        const ty = Math.abs( tangents[ 0 ].y );
-        const tz = Math.abs( tangents[ 0 ].z );
+        normals.push_back(Vector3());
+        binormals.push_back(Vector3()); //= new Vector3();
+        float min = math_number::MAX;
+        const float tx = std::abs( tangents[ 0 ].x );
+        const float ty = std::abs( tangents[ 0 ].y );
+        const float tz = std::abs( tangents[ 0 ].z );
 
         if ( tx <= min ) {
-
             min = tx;
             normal.set( 1, 0, 0 );
-
         }
 
         if ( ty <= min ) {
-
             min = ty;
             normal.set( 0, 1, 0 );
-
         }
 
         if ( tz <= min ) {
-
             normal.set( 0, 0, 1 );
-
         }
 
         vec.crossVectors( tangents[ 0 ], normal ).normalize();
@@ -265,50 +254,37 @@ public:
         normals[ 0 ].crossVectors( tangents[ 0 ], vec );
         binormals[ 0 ].crossVectors( tangents[ 0 ], normals[ 0 ] );
 
-
         // compute the slowly-varying normal and binormal vectors for each segment on the curve
-
-        for ( let i = 1; i <= segments; i ++ ) {
-
-            normals[ i ] = normals[ i - 1 ].clone();
-
-            binormals[ i ] = binormals[ i - 1 ].clone();
+        for ( int i = 1; i <= segments; i ++ ) {
+            normals.push_back(normals[ i - 1 ].clone());
+            //binormals[ i ] = binormals[ i - 1 ].clone();
+            binormals.push_back(binormals[ i - 1 ].clone());
 
             vec.crossVectors( tangents[ i - 1 ], tangents[ i ] );
-
-            if ( vec.length() > Number.EPSILON ) {
-
+            if ( vec.length() > math_number::EPSILON ) {
                 vec.normalize();
-
-                const theta = Math.acos( MathUtils.clamp( tangents[ i - 1 ].dot( tangents[ i ] ), - 1, 1 ) ); // clamp for floating pt errors
+                const float theta = std::acos( math::clamp<float>( tangents[ i - 1 ].dot( tangents[ i ] ), - 1, 1 ) ); // clamp for floating pt errors
 
                 normals[ i ].applyMatrix4( mat.makeRotationAxis( vec, theta ) );
-
             }
 
             binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
-
         }
 
         // if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
+        if ( closed == true ) {
 
-        if ( closed === true ) {
-
-            let theta = Math.acos( MathUtils.clamp( normals[ 0 ].dot( normals[ segments ] ), - 1, 1 ) );
+            float theta = std::acos( math::clamp<float>( normals[ 0 ].dot( normals[ segments ] ), - 1, 1 ) );
             theta /= segments;
 
             if ( tangents[ 0 ].dot( vec.crossVectors( normals[ 0 ], normals[ segments ] ) ) > 0 ) {
-
                 theta = - theta;
-
             }
 
-            for ( let i = 1; i <= segments; i ++ ) {
-
+            for ( int i = 1; i <= segments; i ++ ) {
                 // twist a little...
                 normals[ i ].applyMatrix4( mat.makeRotationAxis( tangents[ i ], theta * i ) );
                 binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
-
             }
 
         }
