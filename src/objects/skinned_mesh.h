32c1af6120f797ce783fdcc97ded641047d5ee85
nintymiles <nintymiles@icloud.com>
2024-04-01 23:07:34 +0800

    Update skinned_mesh.h

--- src/objects/skinned_mesh.h
+++ src/objects/skinned_mesh.h
@@ -26,6 +26,19 @@ public:
     SkinnedMesh(const Geometry::sptr& geometry, const std::initializer_list<Material::sptr>& materials);
 
     SkinnedMesh(const SkinnedMesh& mesh);
+
+    SkinnedMesh& bind( Skeleton::sptr skeleton, Matrix4& bindMatrix );
+
+    SkinnedMesh& pose(){
+        this->skeleton->pose();
+        return *this;
+    }
+
+    SkinnedMesh& normalizeSkinWeights();
+
+    void updateMatrixWorld( bool force );
+
+//    boneTransform( index, target )
 };
 
 #endif //THREE_CPP_SKINNED_MESH_H
