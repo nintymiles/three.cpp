7a35bd700132e166b61f1fce9d6f495bdfae16d8
nintymiles <nintymiles@icloud.com>
2024-10-29 23:55:19 +0800

    Update interpolant.h

--- src/math/interpolant.h
+++ src/math/interpolant.h
@@ -62,144 +62,9 @@ public:
         size_t i0 = (i1<=0)?0:(i1-1);
         auto t1 = pp[ i1 ],t0 = pp[ i0 ];
 
-        //auto validate_interval = [&](){
-        {
-            //auto seek = [&](){
-            {
-                size_t right = 0;
-
-                //auto linear_scan = [&](){
-                {
-
-                    //- See http://jsperf.com/comparison-to-undefined/3
-                    //- slower code:
-                    //-
-                    //- 				if ( t >= t1 || t1 === undefined ) {
-                    //auto forward_scan = [&](){
-                    {
-                        if ( ! ( t < t1 ) ) {
-
-                            for (auto giveUpAt = i1 + 2;;) {
-
-                                if (t1 == 0) {
-                                    if (t < t0) break; //forward_scan;
-
-                                    // after end
-                                    i1 = pp.size();
-                                    this->_cachedIndex = i1;
-                                    this->copySampleValue_((i1<=0)?0:(i1-1));
-                                }
-
-                                if (i1 == giveUpAt) break; // this loop
-
-                                t0 = t1;
-                                t1 = pp[++i1];
-
-                                if (t < t1) {
-                                    // we have arrived at the sought interval
-                                    break; //seek;
-                                }
-
-                            }
-
-                            // prepare binary search on the right side of the index
-                            right = pp.size();
-                            return *this;//std::vector<float>{};
-                            //linear_scan;
-                        }
-                    }
-                    //forward_scan();
-
-                    //- slower code:
-                    //-	if ( t < t0 || t0 === undefined ) {
-                    if ( ! ( t >= t0 ) ) {
-
-                        // looping?
-                        auto t1global = pp[ 1 ];
-
-                        if ( t < t1global ) {
-                            i1 = 2; // + 1, using the scan for the details
-                            t0 = t1global;
-
-                        }
-
-                    // linear reverse scan
-                    for ( auto giveUpAt = i1 - 2; ; ) {
-
-                        if ( t0 == 0 ) {
-
-                            // before start
-                            this->_cachedIndex = 0;
-                            this->copySampleValue_( 0 );
-
-                        }
-
-                        if ( i1 == giveUpAt ) break; // this loop
-
-                        t1 = t0;
-                        t0 = pp[ -- i1 - 1 ];
-
-                        if ( t >= t0 ) {
-                            // we have arrived at the sought interval
-                            break;
-                        }
-
-                    }
-
-                    // prepare binary search on the left side of the index
-                    right = i1;
-                    i1 = 0;
-                    //break linear_scan;
-                    return *this;//std::vector<float>();
-                }
-
-                // the interval is valid
-                //break validate_interval;
-                //return *this;//std::vector<float>();
-
-            } // linear scan
-            //linear_scan();
-
-            // binary search
-            while ( i1 < right ) {
-                auto mid = ( i1 + right ) >> 1;
-
-                if ( t < pp[ mid ] ) {
-                    right = mid;
-                } else {
-                    i1 = mid + 1;
-                }
-            }
-            //todo:fix it
-            if(i1>=pp.size()) i1 = pp.size()-1;
-            t1 = pp[ i1 ];
-            t0 = pp[ i1 - 1 ];
-
-            // check boundary cases, again
-            if ( t0 == 0 ) {
-                this->_cachedIndex = 0;
-                this->copySampleValue_( 0 );
-                return *this;
-            }
-
-            if ( t1 == 0 ) {
-                i1 = pp.size();
-                this->_cachedIndex = i1;
-                this->copySampleValue_( i1 - 1 );
-                return *this;
-            }
-
-            } // seek
-            //seek();
-
-            this->_cachedIndex = i1;
-
-            this->intervalChanged_( i1, t0, t1 );
-
-        }; // validate_interval
-        //validate_interval();
-
-        this->interpolate_( i1, t0, t, t1 );
+        if(validate_interval(t,t0,t1)) {
+            this->interpolate_(i1, t0, t, t1);
+        }
 
         return *this;
     }
@@ -235,6 +100,160 @@ protected:
 
         return result;
     }
+
+private:
+    //- See http://jsperf.com/comparison-to-undefined/3
+    //- slower code:
+    //-
+    //- 				if ( t >= t1 || t1 === undefined ) {
+    //auto forward_scan = [&](){
+    bool forward_scan(float t,float t0,float t1,size_t &right)
+    {
+        auto& pp = this->parameterPositions;
+        auto i1 = this->_cachedIndex;
+
+        if ( ! ( t < t1 ) ) {
+
+            for (auto giveUpAt = i1 + 2;;) {
+
+                if (t1 == 0) {
+                    if (t < t0) break; //forward_scan;
+
+                    // after end
+                    i1 = pp.size();
+                    this->_cachedIndex = i1;
+                    this->copySampleValue_((i1<=0)?0:(i1-1));
+                }
+
+                if (i1 == giveUpAt) break; // this loop
+
+                t0 = t1;
+                t1 = pp[++i1];
+
+                if (t < t1) {
+                    // we have arrived at the sought interval
+                    break; //seek;
+                }
+
+            }
+
+            // prepare binary search on the right side of the index
+            right = pp.size();
+            //return *this;//std::vector<float>{};
+            return true;
+            //linear_scan;
+        }
+        return true;
+    } //forward_scan();
+
+    bool linear_scan(float t,float t0,float t1,size_t &right){
+        auto& pp = this->parameterPositions;
+        auto i1 = this->_cachedIndex;
+
+        if(forward_scan(t,t0,t1,right)){
+
+            //- slower code:
+            //-	if ( t < t0 || t0 === undefined ) {
+            if (!(t >= t0)) {
+
+                // looping?
+                auto t1global = pp[1];
+
+                if (t < t1global) {
+                    i1 = 2; // + 1, using the scan for the details
+                    t0 = t1global;
+
+                }
+
+                // linear reverse scan
+                for (auto giveUpAt = i1 - 2;;) {
+
+                    if (t0 == 0) {
+
+                        // before start
+                        this->_cachedIndex = 0;
+                        this->copySampleValue_(0);
+
+                    }
+
+                    if (i1 == giveUpAt) break; // this loop
+
+                    t1 = t0;
+                    t0 = pp[--i1 - 1];
+
+                    if (t >= t0) {
+                        // we have arrived at the sought interval
+                        break;
+                    }
+
+                }
+
+                // prepare binary search on the left side of the index
+                right = i1;
+                i1 = 0;
+                //break linear_scan;
+                return true;//std::vector<float>();
+            }
+            return true;
+            // the interval is valid
+            //break validate_interval;
+            //return *this;//std::vector<float>();
+        }
+
+    } // linear scan
+    //linear_scan();
+
+    bool seek(float t,float t0,float t1){
+        auto& pp = this->parameterPositions;
+        auto i1 = this->_cachedIndex;
+
+        size_t right = 0;
+        if(linear_scan(t,t0,t1,right)) {
+
+            // binary search
+            while (i1 < right) {
+                auto mid = (i1 + right) >> 1;
+
+                if (t < pp[mid]) {
+                    right = mid;
+                } else {
+                    i1 = mid + 1;
+                }
+            }
+            //todo:fix it
+            if (i1 >= pp.size()) i1 = pp.size() - 1;
+            t1 = pp[i1];
+            t0 = pp[i1 - 1];
+
+            // check boundary cases, again
+            if (t0 == 0) {
+                this->_cachedIndex = 0;
+                this->copySampleValue_(0);
+                return true;
+            }
+
+            if (t1 == 0) {
+                i1 = pp.size();
+                this->_cachedIndex = i1;
+                this->copySampleValue_(i1 - 1);
+                return true;
+            }
+        }
+        return true;
+    } // seek
+    //seek();
+
+    bool validate_interval(float t,float t0,float t1){
+        auto& pp = this->parameterPositions;
+        auto i1 = this->_cachedIndex;
+
+        if(seek(t,t0,t1)) {
+            this->_cachedIndex = i1;
+            this->intervalChanged_(i1, t0, t1);
+        }
+        return true;
+    }; // validate_interval
+    //validate_interval();
 
 };
 
